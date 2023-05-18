897a595e273e0b5203c5928b4a488686edea0147
paramjeetbankra <58567520+paramjeetbankra@users.noreply.github.com>
2023-05-18 20:14:16 +0530

    Update Hero.js

--- src/components/Landing/Hero.js
+++ src/components/Landing/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className={styles.herobg}>
       <div className={styles.heroDiv}>
-        <img alt="" className={styles.img} />
+        <img alt="" className={styles.img} src= "../photos/images.jpeg" />
       </div>
     </div>
   );
